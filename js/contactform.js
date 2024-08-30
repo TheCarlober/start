5251ef5774879db33a6fbebcce604cf2cd9ced00
Carlober_ <86197463+TheCarlober@users.noreply.github.com>
2024-08-30 05:09:06 +0200

    Update contactform.js

--- js/contactform.js
+++ js/contactform.js
@@ -2,7 +2,7 @@ document.getElementById('contact-form').addEventListener('submit', async functio
     e.preventDefault();
     const botToken = '7499023185:AAEyvK2jO-5v6xTzQWcgwRVFLjpvVAluh5U'
     const chatId = '1651916414';
-    const text = `Tienes un Nuevo mensaje:\n\nNombe: ${this.name.value}
+    const text = `Tienes un Nuevo mensaje:\n\nNombre: ${this.name.value}
     \nCorreo Electrónico: ${this.email.value}
     \n\nMensaje: ${this.message.value}`;
     const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`,{
@@ -10,7 +10,7 @@ document.getElementById('contact-form').addEventListener('submit', async functio
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ chat_id: chatId, text: text })
     });
-    if (response.ok) window.location.href = "https://thecarlober.github.io/start/";
+    if (response.ok) window.location.href = "https://thecarlober.github.io/start/pages/Success.html";
 
 
 });
